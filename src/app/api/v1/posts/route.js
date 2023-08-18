a753d4ad04bb0a873032e0b887c06461a88d655b
hamsterster <xhrrl003@gmail.com>
2023-08-18 17:38:05 +0900

    Refactor: 초기화

--- src/app/api/v1/posts/route.js
+++ src/app/api/v1/posts/route.js
@@ -1,5 +1,54 @@
+import mongoose from 'mongoose';
+import createError from 'http-errors';
 import { NextResponse } from 'next/server';
 
-export const Get = async (request) => {
-  return new NextResponse('It works!', { status: 200 });
-};
+import dbConnect from '@lib/dbConnect';
+import Post from '@models/Post';
+import { ERROR_MESSAGES, ERROR_CODES } from '@utils/errors';
+import { sendErrorResponse } from '@utils/response';
+import { validateUserId } from '@utils/validateUserId';
+
+/**
+ * 포스트 목록 조회 API
+ * @URL /api/v1/posts?userId=:userId&page={page_number}&limit={items_per_page}
+ * @param request
+ */
+async function GET(request) {
+  await dbConnect();
+  const successResponse = {
+    status: 'success',
+  };
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get('userId');
+    const page = searchParams.get('page');
+    const limit = searchParams.get('limit');
+
+    if (userId) {
+      validateUserId(userId);
+    }
+
+    if (!page || !limit) {
+      throw createError(
+        ERROR_CODES.MISSING_PARAMETERS,
+        ERROR_MESSAGES.MISSING_PARAMETERS,
+      );
+    }
+    const findOption = userId ? { author: userId } : {};
+    const posts = await Post.find(findOption, null, {
+      skip: (page - 1) * limit,
+      limit,
+    }).exec();
+    successResponse.data = posts;
+
+    const totalPosts = await Post.countDocuments(findOption);
+    successResponse.totalPosts = totalPosts;
+
+    return NextResponse.json(successResponse);
+  } catch (error) {
+    return sendErrorResponse(error);
+  }
+}
+
+export { GET };
--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,22 +1,6 @@
-'use client';
-
-import React, { useEffect } from 'react';
-import authService from '../../components/Service/authService';
 import Link from 'next/link';
-import { onLogin } from '../../components/Login/login';
 
 function LoginPage() {
-  useEffect(() => {
-    const unsubscribe = authService.auth.onAuthStateChanged((user) => {
-      if (user) {
-        window.location.href = '/';
-      } else {
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
   return (
     <div className='text-center font-sans'>
       <div className='mt-32 mb-20'>
@@ -25,10 +9,7 @@ function LoginPage() {
         </Link>
       </div>
       <div>
-        <button
-          onClick={onLogin}
-          className='text-xl text-white font-bold bg-[#0044ff] rounded-lg hover:bg-[#0000ff] py-2 px-8'
-        >
+        <button className='text-xl text-white font-bold bg-[#0044ff] rounded-lg hover:bg-[#0000ff] py-2 px-8'>
           google login
         </button>
       </div>
@@ -43,4 +24,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export { LoginPage };
