8480e7e5767399f4b5044ce371c340afa1794352
Josh5519 <AcForCompany@gmail.com>
2023-08-25 20:15:38 +0900

    Chore: amplify 배포 테스트

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 export { default } from 'next-auth/middleware';
 
 // login authentication 미들웨어
+// 배포 테스트
 // https://next-auth.js.org/configuration/nextjs
 export const config = { matcher: ['/profile/(.*)', '/post/editor/(.*)'] };
