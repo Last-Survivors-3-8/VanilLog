73479fe4239a3d0d78468bc81322ebe16c5f1cce
sonji406 <wkaqh24@gmail.com>
2023-08-27 04:11:16 +0900

    Test: 로그인 커스텀 명령어에 추가

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,33 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginWithGoogle', () => {
+  return cy
+    .visit('/auth/login')
+    .get('#loginButton')
+    .should('be.visible')
+    .click()
+    .then(() => {
+      const socialLoginOptions = {
+        username: Cypress.env('GOOGLE_USER'),
+        password: Cypress.env('GOOGLE_PW'),
+        loginUrl: `${Cypress.env('SITE_NAME')}/auth/login`,
+        headless: false,
+        logs: true,
+        isPopup: true,
+        loginSelector: '#googleLoginButton',
+        postLoginSelector: '#logoutButton',
+      };
+
+      return cy.task('GoogleSocialLogin', socialLoginOptions, {
+        timeout: 220000,
+      });
+    })
+    .then(({ cookies }) => {
+      const cookie = cookies.find(
+        (cookie) => cookie.name === Cypress.env('COOKIE_NAME'),
+      );
+      if (cookie) {
+        return cy.setCookie(cookie.name, cookie.value).then(() => {
+          return cookie.value;
+        });
+      }
+    });
+});
