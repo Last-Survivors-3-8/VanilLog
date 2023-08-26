3130294715ac2e1b6816ee2370c56a747e3e8367
sonji406 <wkaqh24@gmail.com>
2023-08-27 04:10:32 +0900

    Test: 포스트 API 수정 및 정리

--- cypress/e2e/api/v1/post/route.cy.js
+++ cypress/e2e/api/v1/post/route.cy.js
@@ -1,126 +1,118 @@
-describe('포스트 관련 API 테스트', function () {
+describe('포스트 관련 API 테스트', () => {
   const apiUrl = '/api/v1/post';
-  const test_author_id = '64e8fed80be7b91ec1a4ecad';
-  const testTitle = '서버 유닛 테스트 POST';
-  const testContent = {
+
+  const authorId = '64e8fed80be7b91ec1a4ecad';
+  const title = '서버 유닛 테스트 POST';
+  const content = {
     time: { $numberDouble: '1692806018049.0' },
     blocks: [
       { id: 'mDjmDzvpg8', type: 'paragraph', data: { text: '테스트 내용' } },
     ],
     version: '2.27.2',
   };
+
   let postId;
   let mergedUrl;
   let cookieValue;
 
-  // 로그인 관련 설정
   before(() => {
-    cy.visit('/auth/login');
-    cy.get('#loginButton').should('be.visible').click();
-    const socialLoginOptions = {
-      username: Cypress.env('GOOGLE_USER'),
-      password: Cypress.env('GOOGLE_PW'),
-      loginUrl: `${Cypress.env('SITE_NAME')}/auth/login`,
-      headless: false,
-      logs: true,
-      isPopup: true,
-      loginSelector: '#googleLoginButton',
-      postLoginSelector: '#logoutButton',
-    };
-
-    cy.task('GoogleSocialLogin', socialLoginOptions, { timeout: 220000 }).then(
-      ({ cookies }) => {
-        const cookie = cookies.find(
-          (cookie) => cookie.name === Cypress.env('COOKIE_NAME'),
-        );
-        if (cookie) {
-          cy.setCookie(cookie.name, cookie.value);
-          cookieValue = cookie.value;
-        }
-      },
-    );
-  });
-
-  // 포스트 생성 전 설정
-  before(() => {
-    cy.request({
-      method: 'POST',
-      url: apiUrl,
-      body: { title: testTitle, content: testContent, author: test_author_id },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      postId = response.body.data._id;
-      mergedUrl = `${apiUrl}/${postId}`;
+    cy.loginWithGoogle().then((cookie) => {
+      cookieValue = cookie;
     });
   });
 
-  // 본문 파라미터 누락 테스트
-  it('본문 파라미터 누락 테스트 400', () => {
-    cy.request({
-      method: 'POST',
-      url: apiUrl,
-      body: { title: testTitle, content: testContent, author: '' },
-    }).then((response) => {
-      expect(response.body.status).to.eq(400);
-      expect(response.body.message).to.eq('필요한 파라미터가 부족합니다.');
+  context('포스트 생성 API 테스트', () => {
+    it('파라미터 누락 테스트 400', () => {
+      cy.request({
+        method: 'POST',
+        url: apiUrl,
+        body: { title: title, content: content, author: '' },
+      }).then((response) => {
+        expect(response.body.status).to.eq(400);
+        expect(response.body.message).to.eq('필요한 파라미터가 부족합니다.');
+      });
+    });
+
+    it('authorId 형식 테스트 401', () => {
+      cy.request({
+        method: 'POST',
+        url: apiUrl,
+        body: { title: title, content: content, author: 'userId' },
+      }).then((response) => {
+        expect(response.body.status).to.eq(401);
+        expect(response.body.message).to.eq('userId 형식이 일치하지 않습니다.');
+      });
+    });
+
+    it('생성 성공 테스트', () => {
+      cy.request({
+        method: 'POST',
+        url: apiUrl,
+        body: { title: title, content: content, author: authorId },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.status).to.eq('success');
+        expect(response.body.data.title).to.eq(title);
+        expect(response.body.data.content).to.deep.eq(content);
+        expect(response.body.data.author).to.eq(authorId);
+        postId = response.body.data._id;
+        mergedUrl = `${apiUrl}/${postId}`;
+      });
     });
   });
 
-  // userId 형식 테스트
-  it('userId 형식 테스트 401', () => {
-    cy.request({
-      method: 'POST',
-      url: apiUrl,
-      body: { title: testTitle, content: testContent, author: 'userId' },
-    }).then((response) => {
-      expect(response.body.status).to.eq(401);
-      expect(response.body.message).to.eq('userId 형식이 일치하지 않습니다.');
+  context('포스트 조회 API 테스트', () => {
+    it('포스트 조회 성공 테스트', () => {
+      cy.request({ method: 'GET', url: mergedUrl }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.status).to.eq('success');
+        expect(response.body.data._id).to.eq(postId);
+      });
     });
   });
 
-  it('포스트 생성 성공 테스트 200', () => {
-    cy.request({
-      method: 'POST',
-      url: apiUrl,
-      body: { title: testTitle, content: testContent, author: test_author_id },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.data.title).to.eq(testTitle);
-      expect(response.body.data.content).to.deep.eq(testContent);
-      expect(response.body.data.author).to.eq(test_author_id);
+  context('포스트 수정 API 테스트', () => {
+    it('포스트 수정 성공 테스트', () => {
+      const updateTitle = '수정 테스트 제목';
+      const updateContent = {
+        time: { $numberDouble: '1692806018049.0' },
+        blocks: [
+          {
+            id: 'mDjmDzvpg8',
+            type: 'paragraph',
+            data: { text: '업데이트 테스트 내용' },
+          },
+        ],
+        version: '2.27.2',
+      };
+
+      cy.request({
+        method: 'PUT',
+        url: mergedUrl,
+        headers: { Cookie: `${Cypress.env('COOKIE_NAME')}=${cookieValue}` },
+        body: { title: updateTitle, content: updateContent },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.status).to.eq('success');
+        expect(response.body.data.post.title).to.eq(updateTitle);
+        expect(response.body.data.post.content).to.deep.eq(updateContent);
+      });
     });
   });
 
-  it('포스트 조회 성공 테스트', () => {
-    cy.request({ method: 'GET', url: mergedUrl }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.data._id).to.eq(postId);
-    });
-  });
-
-  it('포스트 수정 성공 테스트', () => {
-    const updateTestTitle = '수정 테스트 제목';
-    const updateTestContent = {
-      time: { $numberDouble: '1692806018049.0' },
-      blocks: [
-        {
-          id: 'mDjmDzvpg8',
-          type: 'paragraph',
-          data: { text: '업데이트 테스트 내용' },
+  context('포스트 삭제 API 테스트', () => {
+    it('포스트 삭제 테스트', () => {
+      cy.request({
+        method: 'DELETE',
+        url: mergedUrl,
+        headers: {
+          Cookie: `${Cypress.env('COOKIE_NAME')}=${cookieValue}`,
         },
-      ],
-      version: '2.27.2',
-    };
-
-    cy.request({
-      method: 'PUT',
-      url: mergedUrl,
-      headers: { Cookie: `${Cypress.env('COOKIE_NAME')}=${cookieValue}` },
-      body: { title: updateTestTitle, content: updateTestContent },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.data.post.title).to.eq(updateTestTitle);
-      expect(response.body.data.post.content).to.deep.eq(updateTestContent);
+        body: {},
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.status).to.eq('success');
+      });
     });
   });
 });
